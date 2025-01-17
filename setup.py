6ccc1a0bf3aa2c7241421eabce7354cac1eced30
John Onofrey <jonofrey@YLS-HMSYQ34.yu.yale.edu>
2025-01-17 12:03:58 -0500

    added histopath example

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='segmentation-plot',
-    version='0.1',
+    version='0.2',
     author='Onofrey Lab',
     author_email='john.onofrey@yale.edu',
     description='Visualize image segmentations as contours using matplotlib.',
@@ -14,5 +14,5 @@ setuptools.setup(
     url='https://github.com/OnofreyLab/segmentation-plot',
     license='MIT',
     packages=find_packages(),
-    package_data={"segmentation_viz": ["py.typed"]},
+    package_data={"segmentation_plot": ["py.typed"]},
 )
